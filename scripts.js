89852c76b2c4014fc4b9091a4fc53994a0f16195
Larissalaub <larilaube05@gmail.com>
2024-09-10 19:17:55 -0300

    arruamndo a semiapi

--- scripts.js
+++ scripts.js
@@ -1,29 +1,34 @@
-const itemForm =  document.getElementById('item-form');
-const itemList = document.getElementById('item-List');
+const itemForm = document.getElementById('item-form');
+const itemList = document.getElementById('item-list');
 const totalAmount = document.getElementById('total-amount');
 
 let total = 0;
 
 itemForm.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+    event.preventDefault();
 
-    const itemName = document.getElementById('item-name');
-    const itemQuatity = parseInt('item-quatity').value);
+    const itemName = document.getElementById('item-name').value;
+    const itemQuantity = parseInt(document.getElementById('item-quantity').value);
     const itemPrice = parseFloat(document.getElementById('item-price').value);
 
-    const itemTotalPrice = itemQuatity * itemPrice;
+    const itemTotalPrice = itemQuantity * itemPrice;
 
-    const lisItem = dispatchEvent.createElement('li');
-    lisItem.innerHTML = `
-    ${itemName} - ${itemQuantity} unidade(s) - R$ ${itemTotalPrice.toFixed(2)}
-    <button class="delete-btn">Remover</button>
-`;
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+        ${itemName} - ${itemQuantity} unidade(s) - R$ ${itemTotalPrice.toFixed(2)}
+        <button class="delete-btn">Remover</button>
+    `;
 
-itemList.appendChild(lisItem);
+    itemList.appendChild(listItem);
 
-total += itemTotalPrice;
-totalAmount.textContent = total.toFixed(2);
+    total += itemTotalPrice;
+    totalAmount.textContent = total.toFixed(2);
 
-itemForm.reset();
+    itemForm.reset();
 
-}
+    listItem.querySelector('.delete-btn').addEventListener('click', function() {
+        itemList.removeChild(listItem);
+        total -= itemTotalPrice;
+        totalAmount.textContent = total.toFixed(2);
+    });
+});
